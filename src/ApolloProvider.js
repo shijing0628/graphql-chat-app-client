50df57e6ffdb57e0face8c8464001c8dcb4421e4
shijing0628 <brendaliu0628@gmail.com>
2021-02-08 14:12:08 -0600

    login and register page client

---
+++ src/ApolloProvider.js
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from '@apollo/client';
+import React from 'react'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
+export default function ApolloProvider(props){
+ return (<Provider client={client} {...props}/>)
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import {Container} from 'react-bootstrap'
+import ApolloProvider from './ApolloProvider'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Login from './pages/Login'
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider>
+      <BrowserRouter>
+     <Container className="pt-5">
+       <Switch>
+      <Route path="/" component={Home} exact/>
+     <Route path="/register" component={Register}/>
+     <Route path="/login" component={Login}/>
+     </Switch>
+    </Container>
+    </BrowserRouter>
+   </ApolloProvider>
   );
 }
 
