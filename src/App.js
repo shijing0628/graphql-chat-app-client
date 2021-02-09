a6bea2422ac0887ebaf41222cd9e1be7c8758e71
shijing0628 <brendaliu0628@gmail.com>
2021-02-09 11:25:34 -0600

    send a message to selected user

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import ApolloProvider from "./ApolloProvider";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Register from "./pages/Register";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/Login";
-
+import { MessageProvider } from "./context/message";
 import { AuthProvider } from "./context/auth";
 import DynamicRoute from './util/DynamicRoute'
 
@@ -14,6 +14,7 @@ function App() {
   return (
     <ApolloProvider>
       <AuthProvider>
+        <MessageProvider>
         <BrowserRouter>
           <Container className="pt-5">
             <Switch>
@@ -23,6 +24,7 @@ function App() {
             </Switch>
           </Container>
         </BrowserRouter>
+        </MessageProvider>
       </AuthProvider>
     </ApolloProvider>
   );
